3b41c9009cdd91215b42237450c7a937763bff06
lim3nd <lim3nd@gmail.com>
2020-02-04 14:32:44 +0100

    code

--- Example2 Drag & Drop/main.js
+++ Example2 Drag & Drop/main.js
@@ -1,1 +1,28 @@
 import ZingTouch from 'zingtouch';
+
+let zt = new ZingTouch.Region(document.body);
+
+let dragObj = document.getElementById("my-div");
+
+zt.bind(dragObj, "pan", function(e){
+
+}, false);
+
+new ZingTouch.Pan({
+	numInputs: 1
+})
+
+class destinationFolder{
+    constructor(x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    draw(){
+        element = document.createElement(my-div);
+    }
+}
+
+let folder = new destinationFolder(5, 5, 100, 100);
+folder.draw();
