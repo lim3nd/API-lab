eb7c8b8d4d71b6d44e47c4b78cd719445754e5ab
Ivar Lundin <60367191+ivarlundin@users.noreply.github.com>
2020-02-04 22:51:21 +0100

    started to add lib to example

--- Example 1 Map-Pin/main.js
+++ Example 1 Map-Pin/main.js
@@ -1,1 +1,16 @@
-import ZingTouch from 'zingtouch';
+var ZingTouch = require('zingtouch');
+
+var zt = new ZingTouch.Region(document.getElementById("touchArea"));
+
+var myElement = document.getElementById('my-div');
+
+zt.bind(myElement, 'pan', function(e){
+	console.log(e);
+}, false);
+
+
+
+//Set the window height
+document.getElementsByTagName("body")[0].style.height = window.innerHeight + "px"
+//Resizing of window 
+window.addEventListener("resize", () => { document.getElementsByTagName("body")[0].style.height = window.innerHeight + "px";; }, false);
