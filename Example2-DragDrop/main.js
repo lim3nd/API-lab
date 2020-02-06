5a43dce815eb9d3ad0ec3b761e42ba27ad4674dd
lim3nd <lim3nd@gmail.com>
2020-02-07 00:52:33 +0100

    update ex2

--- Example2-DragDrop/main.js
+++ Example2-DragDrop/main.js
@@ -1,29 +1,11 @@
-let ZingTouch = require('zingtouch');
+console.log("Running....");
 
+const ZingTouch = require('zingtouch');
 
-let zt = new ZingTouch.Region(document.body);
+let touchArea = document.getElementById("toucharea");
+let myRegion = new ZingTouch.Region(document.body);
+const pan = new ZingTouch.Pan();
 
-let dragObj = document.getElementById("my-div");
-
-zt.bind(dragObj, "pan", function(e){
-
-}, false);
-
-new ZingTouch.Pan({
-	numInputs: 1
-})
-
-class destinationFolder{
-    constructor(x, y, width, height){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-    draw(){
-        element = document.createElement(my-div);
-    }
-}
-
-let folder = new destinationFolder(5, 5, 100, 100);
-folder.draw();
+myRegion.bind(touchArea, "pan", function(e){
+  console.log(e.detail);
+});
