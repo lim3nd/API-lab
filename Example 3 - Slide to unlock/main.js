2caff029d27b8bef88dcb60f0d12ec195b97c2ba
Ivar Lundin <60367191+ivarlundin@users.noreply.github.com>
2020-02-18 18:03:09 +0100

    Created some basic graphics for example

--- Example 3 - Slide to unlock/main.js
+++ Example 3 - Slide to unlock/main.js
@@ -1,1 +1,21 @@
+let slider = document.getElementById("arrow");
 
+function moveSlider(x) {
+    let movement = x;
+    //accepted range
+    if (movement > 7 && movement < 312) {
+        slider.style.marginLeft = movement + "px";  
+    } 
+
+    //debug
+    if (x == "max") {
+        slider.style.marginLeft = "312px";
+    }
+    /* else {
+        slider.style.margin-left = movement "312px";
+    }
+    */
+}
+function resetSlider() {
+    slider.style.marginLeft = "7px"; 
+}
