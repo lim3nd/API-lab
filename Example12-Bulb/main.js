fa7a42bed79199a741b49f2bab5a588d7ff62e4a
Rasha Ahmed <ahmed.rasha54@gmail.com>
2020-02-21 00:12:08 +0100

    It's almost working

--- Example12-Bulb/main.js
+++ Example12-Bulb/main.js
@@ -3,12 +3,26 @@ const knob = document.getElementById("knob");
 const bulb = document.getElementById("bulb");
 let angle = 0;
 let currentRotateEventAngle = 0;
-region.bind(knob, "rotate", function(e) {
-  //Actions here
-  const rotateEventAngle = e.detail.angle;
-  if (rotateEventAngle > currentRotateEventAngle) {
-    angle += 5;
-  } else {
-    angle -= 5;
-  }
-});
+region.bind(
+  knob,
+  "rotate",
+  function(e) {
+    //Actions here
+    const rotateEventAngle = e.detail.angle;
+    if (rotateEventAngle > currentRotateEventAngle) {
+      angle += 5;
+    } else {
+      angle -= 5;
+    }
+    currentRotateEventAngle = rotateEventAngle;
+    if (angle > 180 || angle < 0) {
+      return;
+    }
+    knob.style.setProperty("transform", `rotate(${-angle}deg)`);
+    const r = 255;
+    const g = Math.max(Math.min(angle, 255), 247);
+    const b = Math.max(Math.min(angle, 194), 0);
+    bulb.style.setProperty("background-color", `rgb(${r},${g},${b})`);
+  },
+  false
+);
