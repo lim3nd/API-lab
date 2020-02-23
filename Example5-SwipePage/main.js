0560f71c3c7b12103050408ab5709711fac13c2a
Emil Nilsson Delborg <60610339+lim3nd@users.noreply.github.com>
2020-02-23 19:42:24 +0100

    Code update ex5

--- Example5-SwipePage/main.js
+++ Example5-SwipePage/main.js
@@ -1,20 +1,48 @@
-let pageCounter = 1;
+let pageCounter = 0;
 
 const activeRegion = new ZingTouch.Region(document.body);
 let touchArea = document.getElementById("touch-area");
+let pageNumber = document.getElementById("number");
+let headLine = document.getElementById("headline");
+let text = document.getElementById("text");
 const swipe = new ZingTouch.Swipe({
-  escapeVelocity: 0.1,
+  escapeVelocity: 0.05,
   maxRestTime: 100  
 });
+
+function pageChanger(){
+  if(pageCounter == 0){
+    pageNumber.innerHTML = "Page 1";
+    headLine.innerHTML = "Swipe to Change Page";
+    text.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  }else if(pageCounter == 1){
+    pageNumber.innerHTML = "Page 2";
+    headLine.innerHTML = "<br>";
+    text.innerHTML = "Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam.";
+
+  }else if(pageCounter == 2){
+    pageNumber.innerHTML = "Page 3";
+    headLine.innerHTML = "<br>";
+    text.innerHTML = "Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.";
+  }
+}
 
 activeRegion.bind(touchArea, swipe, function(event){
     console.log(event.detail);
     console.log(event.detail.data[0].currentDirection);
-    if(event.detail.data[0].currentDirection == 360){
-      pageCounter++;
+    if((315 <= event.detail.data[0].currentDirection && event.detail.data[0].currentDirection <= 360) || (0 <= event.detail.data[0].currentDirection && event.detail.data[0].currentDirection <= 45)){
+      if(pageCounter >= 1){
+        console.log("minus")
+        pageCounter--;
+      }
       console.log(pageCounter);
-    }else if(event.detail.data[0].currentDirection == 180){
-      pageCounter--;
+      pageChanger();
+    }else if(135 <= event.detail.data[0].currentDirection <= 225){
+      if(pageCounter < 2){
+        console.log("plus")
+        pageCounter++;
+      }
       console.log(pageCounter);
+      pageChanger();
     }
 });
