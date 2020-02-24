904f640dc9437973c013e171a1b59e49a63bc1f7
JEhrenbeck <60610224+JEhrenbeck@users.noreply.github.com>
2020-02-24 17:57:06 +0100

    adding more functionality to egg timer + small tweak to saturation changer style
    ^

--- Egg Timer/main.js
+++ Egg Timer/main.js
@@ -3,20 +3,25 @@ var currentAngle = 15;
 //Using a layer on top of the entire page for "fat-finger" detection on mobile devices.
 document.getElementById('rotatable').style.transform = 'rotate(15deg)';
 
+//declaration of variables
 var target = document.getElementById('interaction');
 var region = new ZingTouch.Region(target);
 
+//binds element to gesture
 region.bind(target, 'rotate', function(e) {
   var rotatable = document.getElementById('rotatable');
   currentAngle += e.detail.distanceFromLast;
   rotatable.style.transform = 'rotate(' + currentAngle + 'deg)';
 
-  setOutput([
-    ['Gesture', 'Rotate'],
-    ['angle', Math.floor(e.detail.angle) + "°"],
-    ['distanceFromOrigin', Math.floor(e.detail.distanceFromOrigin) + "°"],
-    ['distanceFromLast', Math.floor(e.detail.distanceFromLast) + "°"]
-  ]);
+  
 
 });
 
+function setTimer(){
+  var numSeconds = (Math.abs(angle) % 360)/360 * 60;
+  window.setTimeout(displayAlert, numSeconds * 1000)
+}
+
+function displayAlert(){
+  alert("Eggs are ready!")
+}
