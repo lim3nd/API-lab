d985050ce80eed9f5f0940d62905ac879d7c3feb
Agustin Guerrero <dibujaman-gamer@hotmail.com>
2020-02-24 14:55:12 +0100

    Final fixes + commentary and cleanup
    Color Wheel Fixed (major bugs removed).
    Commentary added to both JS files.
    Unnecessary code removed.
    Code arrangement improved (more space for easy reading).

--- Example 10 - Volume/main.js
+++ Example 10 - Volume/main.js
@@ -1,11 +1,20 @@
+// Variables
+
 let parent = document.getElementById("volume-box");
 let slider = document.getElementById("volume-bar");
 let counter = document.getElementById("volume-counter");
 let x=0;
+let object = parent.getBoundingClientRect();
+let positionX = object.x;
+let width = object.width;
 
+// Regions
+
+var stopPositionMax = positionX + width - 30;
+var stopPositionMin = positionX + 10;
 var region = new ZingTouch.Region(document.body);
 
-let object = parent.getBoundingClientRect();
+// Get Position of the Volume Bar (x)
 
 function sliderPos(){
 
@@ -14,36 +23,96 @@ function sliderPos(){
     return sliderX;
 
 }
-
-let positionX = object.x;
-let width = object.width;
-
-var stopPositionMax = positionX + width - 30;
-var stopPositionMin = positionX + 10;
-
-setTimeout(function() {
-
-    document.getElementById("volume-box");
-
-}, );
  
+// Assign Pan to Parent
+
 region.bind(parent, "pan",function (event){
 
-    if (sliderPos() >= stopPositionMin && sliderPos() <= stopPositionMax) {
+    if (sliderPos() >= stopPositionMin && sliderPos() <= stopPositionMax) {         // Allow the bar to move if it is inside the region.
     
     x += event.detail.data[0].change.x;
     slider.style.left = `${x}px`;
 
-    console.log(sliderPos());
+    console.log(sliderPos());           // Show position of volume bar.
 
-    if (sliderPos() < stopPositionMin) {
+// Volume Value Display - (+10 volume / 100px)
+
+    if (sliderPos() <= 530) {                                      
+
+        counter.innerHTML = "0";
+
+    }
+
+    else if (sliderPos() >= 531 && sliderPos() <=630 ) { 
+
+        counter.innerHTML = "10";
+
+    }
+
+    else if (sliderPos() >= 631 && sliderPos() <=730 ) { 
+
+        counter.innerHTML = "20";
+
+    }
+
+    else if (sliderPos() >= 731 && sliderPos() <=830 ) { 
+
+        counter.innerHTML = "30";
+
+    }
+
+    else if (sliderPos() >= 831 && sliderPos() <=930 ) { 
+
+        counter.innerHTML = "40";
+
+    }
+
+    else if (sliderPos() >= 931 && sliderPos() <=1030 ) { 
+
+        counter.innerHTML = "50";
+
+    }
+
+    else if (sliderPos() >= 1031 && sliderPos() <=1130 ) { 
+
+        counter.innerHTML = "60";
+
+    }
+
+    else if (sliderPos() >= 1131 && sliderPos() <=1230 ) { 
+
+        counter.innerHTML = "70";
+
+    }
+
+    else if (sliderPos() >= 1231 && sliderPos() <=1330 ) { 
+
+        counter.innerHTML = "80";
+
+    }
+
+    else if (sliderPos() >= 1331 && sliderPos() <=1430 ) { 
+
+        counter.innerHTML = "90";
+
+    }
+
+    else if (sliderPos() >= 1431 && sliderPos() <=1530 ) { 
+
+        counter.innerHTML = "100";
+
+    }
+
+// Region Limits
+
+    if (sliderPos() < stopPositionMin) {            // Left limit
 
         x = 0;
         slider.style.left = `${x}px`;
 
         }
         
-    else if (sliderPos() > stopPositionMax) {
+    else if (sliderPos() > stopPositionMax) {       // Right limit
       
         x = width - 40;
         slider.style.left = `${x}px`;
--- Example 9 - Color Wheel/main.js
+++ Example 9 - Color Wheel/main.js
@@ -1,83 +1,93 @@
-const region = new ZingTouch.Region(document.body);
-const rotatableElement = document.getElementById("rotatable");
-const pointer = document.getElementById("pointer");
+// 
 
-let currentRotateEventAngle = 0;
+const region = new ZingTouch.Region(document.body);
+const rotatableElement = document.getElementById("rotatable");      // Color Wheel.
+const pointer = document.getElementById("pointer");                 // Arrow.
+
+// Variables //
+
+let currentRotateEventAngle = 0;                                    // Set rotation to 0.
 let angle = 0;
+
+// Assign Rotate Motion to Color Wheel //
 
 region.bind( 
  
   rotatableElement, "rotate", function(e) {
 
-    if (condition) {
+    console.log(e.detail);                                          // Display rotation of color wheel.
 
-    console.log(e.detail);
+    const rotateEventAngle = e.detail.angle;                        // Rotation degree.
 
-    const rotateEventAngle = e.detail.angle;
+// Rotation (Clockwise & Counter-Clockwise) //
+
     if (rotateEventAngle > currentRotateEventAngle) {
       angle += 1;
-    } else {
+    } 
+    
+    else {
       angle -= 1;
     }
-    currentRotateEventAngle = rotateEventAngle;
+
+    currentRotateEventAngle = angle;                                // Value for middle circle color change.
 
     rotatableElement.style.transform = `rotate(${-angle}deg)`;
 
-    const circle = document.getElementById("circle");
+    const circle = document.getElementById("circle");               // Middle Circle.
 
-    if (currentRotateEventAngle > 360) {
-      currentRotateEventAngle = currentRotateEventAngle - 360
+    if (angle > 360) {                                              // Restart rotation degrees.
+      angle = angle - 360
     }
 
-    else if (rotateEventAngle >= 0 && rotateEventAngle < 30) {
+// Change Color of Middle Circle //
+
+    else if (angle >= 0 && angle < 30) {
       circle.style.backgroundColor = "#FD5308";
     }
 
-    else if (rotateEventAngle >= 30 && rotateEventAngle < 60) {
+    else if (angle >= 30 && angle < 60) {
       circle.style.backgroundColor = "#FB9902";
     }
 
-    else if (rotateEventAngle >= 60 && rotateEventAngle < 90) {
+    else if (angle >= 60 && angle < 90) {
       circle.style.backgroundColor = "#FABC02";
     }
 
-    else if (rotateEventAngle >= 90 && rotateEventAngle < 120) {
+    else if (angle >= 90 && angle < 120) {
       circle.style.backgroundColor = "#FEFE33";
     }
 
-    else if (rotateEventAngle >= 120 && rotateEventAngle < 150) {
+    else if (angle >= 120 && angle < 150) {
       circle.style.backgroundColor = "#D0EA2B";
     }
 
-    else if (rotateEventAngle >= 150 && rotateEventAngle < 180) {
+    else if (angle >= 150 && angle < 180) {
       circle.style.backgroundColor = "#66B032";
     }
 
-    else if (rotateEventAngle >= 180 && rotateEventAngle < 210) {
+    else if (angle >= 180 && angle < 210) {
       circle.style.backgroundColor = "#0391CE";
     }
 
-    else if (rotateEventAngle >= 210 && rotateEventAngle < 240) {
+    else if (angle >= 210 && angle < 240) {
       circle.style.backgroundColor = "#0247FE";
     }
 
-    else if (rotateEventAngle >= 240 && rotateEventAngle < 270) {
+    else if (angle >= 240 && angle < 270) {
       circle.style.backgroundColor = "#3D01A4";
     }
 
-    else if (rotateEventAngle >= 270 && rotateEventAngle < 300) {
+    else if (angle >= 270 && angle < 300) {
       circle.style.backgroundColor = "#8601AF";
     }
 
-    else if (rotateEventAngle >= 300 && rotateEventAngle < 330) {
+    else if (angle >= 300 && angle < 330) {
       circle.style.backgroundColor = "#A7194B";
     }
 
-    else if (rotateEventAngle >= 330 && rotateEventAngle < 360) {
+    else if (angle >= 330 && angle < 360) {
       circle.style.backgroundColor = "#FE2712";
     }
-
-  }
 
 },
 
